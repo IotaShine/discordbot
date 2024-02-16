844699a05f14069034753579f3e280ad393480ae
IotaShine <theiotashine@gmail.com>
2024-02-16 16:16:25 -0300

    serverinfo modified

--- commands/useful/server.js
+++ commands/useful/server.js
@@ -1,14 +1,26 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("server")
         .setDescription("te tiro la data sobre el server este"),
     async execute(interaction) {
+
+        const { guild } = interaction;
+        const { name, memberCount, createdAt, joinedAt, ownerId } = guild;
+
+        const icon = guild.iconURL();
+
+        const embedDescription = `Nombre: \`${name}\`\nCantidad de miembros: ${memberCount}\nFecha de creación: ${createdAt}\nTe uniste el: ${joinedAt} \nOwner: <@${ownerId}>.`;
+
+        const embed = new EmbedBuilder()
+            .setTitle(`Server info for: ${name}`)
+            .setDescription(embedDescription)
+            .setColor("Random")
+            .setThumbnail(icon);
+
         try {
-            await interaction.reply(
-                `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`,
-            );
+            await interaction.reply({ embeds: [embed] });
         } catch (error) {
             console.log(error);
             await interaction.reply("Salio para el orto esto no se que onda hubo un error");
