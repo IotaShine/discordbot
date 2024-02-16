f93d1d040d2ab9fa0ee632b9b92409f210988cd2
IotaShine <theiotashine@gmail.com>
2024-02-16 16:57:01 -0300

    añadidas escrituras

---
+++ commands/fun/sabiduria.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const escrituras = require("../../helpers/meme/escrituras.json");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("sabiduría")
+        .setDescription("Sabiduría"),
+
+    async execute(interaction) {
+        const escritura = escrituras[Math.floor(Math.random() * escrituras.length)];
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: escritura.titulo })
+            .setDescription(escritura.escritura)
+            .setFooter({ text: `Autor: ${escritura.autor}` })
+            .setColor("Random");
+
+        return await interaction.reply({ embeds: [embed] });
+    },
+};
