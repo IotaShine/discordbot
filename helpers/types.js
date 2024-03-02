8d27976e02d23c04075c34d7a4fbbe7331de7589
IotaShine <theiotashine@gmail.com>
2024-03-02 00:51:23 -0300

    created types.js file for storing jsdoc type anotations

---
+++ helpers/types.js
@@ -0,0 +1,12 @@
+// types.js
+
+/**
+ * @typedef {Object} User
+ * @property {string} user_id - The user's unique identifier
+ * @property {number} xp - The user's experience points
+ * @property {number} level - The user's level
+ * @property {boolean} [isDirty] - Whether the user's data has to be updated in the database
+*/
+
+
+exports.unused = undefined;
