ef63ef0affbcf0b8bbf8145dc7aee72add1d5dfd
IotaShine <theiotashine@gmail.com>
2024-03-03 03:12:08 -0300

    level.js now asks for a target user for the checking

--- commands/useful/level.js
+++ commands/useful/level.js
@@ -5,26 +5,36 @@ const { getUser, logger } = require("../../helpers");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("level")
-        .setDescription("te tiro la data de que nivel sos"),
+        .setDescription("te tiro la data de que nivel sos")
+        .addUserOption((option) =>
+            option
+                .setName("user")
+                .setDescription("El usuario del que queres saber el nivel")
+                .setRequired(true),
+        ),
 
     /**
      * @function
      * @param {import("discord.js").CommandInteraction} interaction
      */
     async execute(interaction) {
-        const user = userCache.get(interaction.user.id);
+        const user = interaction.options.getUser("user");
+        if (user.bot) return interaction.reply("Los bots no tienen nivel salamin");
 
-        if (!user) {
+        const cachedUser = userCache.get(user.id);
+
+        if (!cachedUser) {
             try {
-                const newUser = await getUser(interaction.user.id);
-                userCache.set(interaction.user.id, newUser);
-                return await interaction.reply(`Sos nivel ${newUser.level} master`);
+                const newUser = await getUser(user.id);
+                if (!newUser) return interaction.reply("No se encontró al usuario");
+                userCache.set(user.id, newUser);
+                return await interaction.reply(`El nivel de ${user.username} es ${newUser.level} master`);
             } catch (error) {
                 logger.error(error);
-                return interaction.reply("There was an error while fetching your data");
+                return interaction.reply("There was an error while fetching the data");
             }
         } else {
-            interaction.reply(`Sos nivel ${user.level} master`);
+            return interaction.reply(`El nivel de ${user.username} es ${user.level} master`);
         }
     },
 };
