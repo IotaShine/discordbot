6dc2cb08c5a7de692d0a9e3d70708254dd3acf66
IotaShine <theiotashine@gmail.com>
2024-03-03 06:22:18 -0300

    deploy-commands-global.js reverted

--- deploy-commands-global.js
+++ deploy-commands-global.js
@@ -7,34 +7,43 @@ require("dotenv").config();
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
-async function refreshCommands() {
-    const commands = [];
-    const foldersPath = path.join(__dirname, "commands");
-    const commandFolders = fs.readdirSync(foldersPath);
+const commands = [];
 
-    for (const folder of commandFolders) {
-        const commandsPath = path.join(foldersPath, folder);
-        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
-        for (const file of commandFiles) {
-            const filePath = path.join(commandsPath, file);
-            const command = require(filePath);
-            if ("data" in command && "execute" in command) {
-                commands.push(command.data.toJSON());
-            } else {
-                logger.warn(`The command at ${filePath} is missing a required "data" or "execute" property.`);
-            }
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ("data" in command && "execute" in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            logger.warn(
+                `The command at ${filePath} is missing a required "data" or "execute" property.`,
+            );
         }
-    }
-
-    const rest = new REST().setToken(TOKEN);
-
-    try {
-        logger.info(`Started refreshing ${commands.length} application (/) commands.`);
-        const data = await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-        logger.info(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        logger.error(error);
     }
 }
 
-module.exports = refreshCommands;
+const rest = new REST().setToken(TOKEN);
+
+(async () => {
+    try {
+        logger.info(
+            `Started refreshing ${commands.length} application (/) commands.`,
+        );
+        const data = await rest.put(Routes.applicationCommands(CLIENT_ID), {
+            body: commands,
+        });
+        logger.info(
+            `Successfully reloaded ${data.length} application (/) commands.`,
+        );
+    } catch (error) {
+        logger.error(error);
+    }
+})();
