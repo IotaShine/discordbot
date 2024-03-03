7426a8c1fe460589dbf6607c9e9188a7ac30f602
IotaShine <theiotashine@gmail.com>
2024-03-03 06:15:10 -0300

    deploy commands reverted

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,36 +4,50 @@ const { REST, Routes } = require("discord.js");
 const { logger } = require("./helpers");
 require("dotenv").config();
 
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const TOKEN = process.env.TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CLIENT_ID = process.env.CLIENT_ID;
 
-async function refreshGuildCommands() {
-    const commands = [];
-    const foldersPath = path.join(__dirname, "commands");
-    const commandFolders = fs.readdirSync(foldersPath);
+const commands = [];
 
-    for (const folder of commandFolders) {
-        const commandsPath = path.join(foldersPath, folder);
-        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
-        for (const file of commandFiles) {
-            const filePath = path.join(commandsPath, file);
-            const command = require(filePath);
-            if ("data" in command && "execute" in command) {
-                commands.push(command.data.toJSON());
-            } else {
-                logger.warn(`The command at ${filePath} is missing a required "data" or "execute" property.`);
-            }
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ("data" in command && "execute" in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            logger.warn(
+                `The command at ${filePath} is missing a required "data" or "execute" property.`,
+            );
         }
-    }
-
-    const rest = new REST().setToken(TOKEN);
-
-    try {
-        logger.info(`Started refreshing ${commands.length} application (/) commands.`);
-        const data = await rest.put(Routes.applicationGuildCommand(CLIENT_ID, GUILD_ID), { body: commands });
-        logger.info(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        logger.error(error);
     }
 }
 
-module.exports = refreshGuildCommands;
+const rest = new REST().setToken(TOKEN);
+
+(async () => {
+    try {
+        logger.info(
+            `Started refreshing ${commands.length} application (/) commands.`,
+        );
+
+        const data = await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands },
+        );
+
+        logger.info(
+            `Successfully reloaded ${data.length} application (/) commands.`,
+        );
+    } catch (error) {
+        logger.error(error);
+    }
+})();
--- index.js
+++ index.js
@@ -4,130 +4,122 @@ const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { Player } = require("discord-player");
 const { YoutubeExtractor } = require("@discord-player/extractor");
 require("dotenv").config();
-const { ACTIVITY, STATUS, TOKEN, ENVIROMENT } = process.env;
+const { ACTIVITY, STATUS, TOKEN, CLIENT_ID } = process.env;
 const { logger, createTables, shutdownHandler } = require("./helpers");
 const db = require("./helpers/db/database");
-const refreshCommands = require("./deploy-commands-global");
-const refreshGuildCommands = require("./deploy-commands");
 
 // FIXME - Remove the db from the client and use the db from the helpers/db/database.js
 // FIXME - Fix date and time in docker container
 
 /** Refrescamos los comandos */
 
-async function startBot() {
+if (!TOKEN) {
+    logger.fatal("No token was provided, please provide one in the .env file.");
+    process.exit(1);
+}
 
-    try {
+if (!CLIENT_ID) {
+    logger.fatal("No client id was provided, please provide one in the .env file.");
+    process.exit(1);
+}
 
-        if (ENVIROMENT === "development") {
-            logger.info("Refreshing guild commands");
-            await refreshGuildCommands();
+if (!ACTIVITY) {
+    logger.warn("No activity was provided, the bot will use the default one.");
+}
+
+if (!STATUS) {
+    logger.warn("No status was provided, the bot will use the default one.");
+}
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+/** Importar todos los comandos */
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+/** Iteramos las carpetas */
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+    /** Por cada archivo en la carpeta actual iteramos los comandos y los importamos */
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+
+            logger.info(`Command ${file} was loaded.`);
         } else {
-            logger.info("Refreshing global commands");
-            await refreshCommands();
+            logger.warn(
+                `The command at ${path.join(
+                    filePath,
+                    file,
+                )} couldn't be loaded, It may be missing a 'data' or 'execute' property.`,
+            );
         }
-
-        const client = new Client({
-            intents: [
-                GatewayIntentBits.Guilds,
-                GatewayIntentBits.GuildMessages,
-                GatewayIntentBits.GuildVoiceStates,
-                GatewayIntentBits.MessageContent,
-            ],
-        });
-
-        /** Importar todos los comandos */
-        client.commands = new Collection();
-        const foldersPath = path.join(__dirname, "commands");
-        const commandFolders = fs.readdirSync(foldersPath);
-
-        /** Iteramos las carpetas */
-        for (const folder of commandFolders) {
-            const commandsPath = path.join(foldersPath, folder);
-            const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
-
-            /** Por cada archivo en la carpeta actual iteramos los comandos y los importamos */
-            for (const file of commandFiles) {
-                const filePath = path.join(commandsPath, file);
-                const command = require(filePath);
-
-                if ("data" in command && "execute" in command) {
-                    client.commands.set(command.data.name, command);
-
-                    logger.info(`Command ${file} was loaded.`);
-                } else {
-                    logger.warn(
-                        `The command at ${path.join(
-                            filePath,
-                            file,
-                        )} couldn't be loaded, It may be missing a 'data' or 'execute' property.`,
-                    );
-                }
-            }
-        }
-
-        /** Importar todos los eventos */
-        const eventsPath = path.join(__dirname, "events");
-        const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
-
-        for (const file of eventFiles) {
-            const filePath = path.join(eventsPath, file);
-            const event = require(filePath);
-
-            if (event.once) {
-                client.once(event.name, (...args) => event.execute(...args));
-            } else {
-                client.on(event.name, (...args) => event.execute(...args));
-            }
-        }
-
-        /** Agregamos el reproductor de música */
-        const player = new Player(client, {
-            skipFFmpeg: true,
-            ytdlOptions: {
-                quality: "highestaudio",
-                highWaterMark: 1 << 25,
-            },
-        });
-        player.extractors.register(YoutubeExtractor, {});
-
-        player.on("error", (queue, error) => {
-            logger.error(`Error en la cola ${queue.guild.id}: ${error.message}`);
-        });
-
-        /** Inicializamos y agregamos la base de datos */
-        /** 
-         * FIXME - Remove the db from the client and use the db from the helpers/db/database.js
-         */
-        client.db = db;
-        createTables(db);
-
-        shutdownHandler(client);
-
-        if (!TOKEN) {
-            logger.fatal("No token was provided, please provide one in the .env file.");
-            process.exit(1);
-        }
-
-        /** Login del bot */
-        client
-            .login(TOKEN)
-            .then(() => {
-                client.user.setPresence({
-                    activities: [{ name: ACTIVITY || "with your feelings" }],
-                    status: STATUS || "online",
-                });
-                logger.info("Bot description status changed");
-            })
-            .catch(error => {
-                logger.fatal("There was an error while logging in");
-                logger.fatal(error);
-                process.exit(1);
-            });
-
-    } catch (error) {
-        logger.fatal(error, "There was an error while refreshing the commands");
     }
 }
 
-startBot();
+/** Importar todos los eventos */
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+/** Agregamos el reproductor de música */
+const player = new Player(client, {
+    skipFFmpeg: true,
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+    },
+});
+player.extractors.register(YoutubeExtractor, {});
+
+player.on("error", (queue, error) => {
+    logger.error(`Error en la cola ${queue.guild.id}: ${error.message}`);
+});
+
+/** Inicializamos y agregamos la base de datos */
+/** 
+ * FIXME - Remove the db from the client and use the db from the helpers/db/database.js
+ */
+client.db = db;
+createTables(db);
+
+shutdownHandler(client);
+
+/** Login del bot */
+client
+    .login(TOKEN)
+    .then(() => {
+        client.user.setPresence({
+            activities: [{ name: ACTIVITY || "with your feelings" }],
+            status: STATUS || "online",
+        });
+        logger.info("Bot description status changed");
+    })
+    .catch(error => {
+        logger.fatal("There was an error while logging in");
+        logger.fatal(error);
+        process.exit(1);
+    });
