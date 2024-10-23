016b8a2ab345773197ec9678ec8f5cf21a3defd6
IotaShine <theiotashine@gmail.com>
2024-10-23 11:27:22 -0300

    fixed nitro gif avatar crashing the bot

--- src/commands/useful/avatar.js
+++ src/commands/useful/avatar.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require("discord.js");
 const { logger } = require("../../helpers/");
 
 module.exports = {
@@ -6,28 +6,47 @@ module.exports = {
         .setName("avatar")
         .setDescription("I retrieve the avatar of the target user.")
         .addUserOption(option =>
+            option.setName("target").setDescription("The target user").setRequired(true),
+        )
+        .addNumberOption(option =>
             option
-                .setName("target")
-                .setDescription("The target user")
-                .setRequired(true),
+                .setName("resolution")
+                .setDescription("The resolution the image should be")
+                .setChoices(
+                    { name: "512", value: 512 },
+                    { name: "1024", value: 1024 },
+                    { name: "4096", value: 4096 },
+                ),
         ),
     /** Retrieves the avatar of the target user
      * @param {import("discord.js").CommandInteraction} interaction
      */
     async execute(interaction) {
         try {
+            await interaction.deferReply();
+            /** @type {import("discord.js").User} */
             const user = interaction.options.getUser("target");
 
-            const img = user.avatarURL({ size: 4096, extension: "png" });
-            if (!img) return await interaction.reply("**[ NOTICE ]** I could not find the avatar.");
+            const res = interaction.options.getNumber("resolution") ?? 512;
 
-            await interaction.reply({
-                content: `${interaction.options.getUser("target").username}'s avatar`,
-                files: [img],
+            const img = user.avatarURL({ size: res });
+            if (!img) {
+                return await interaction.followUp("**[ NOTICE ]** I could not find the avatar.");
+            }
+
+            const color = user.accentColor ?? Colors.Green;
+
+            const embed = new EmbedBuilder()
+                .setTitle(`${user.username}'s Avatar`)
+                .setImage(img)
+                .setColor(color);
+
+            await interaction.followUp({
+                embeds: [embed],
             });
         } catch (error) {
             logger.error(error, "Error in avatar command");
-            await interaction.reply("**[ ERROR ]** There was an error retrieving the avatar.");
+            await interaction.followUp("**[ ERROR ]** There was an error retrieving the avatar.");
         }
     },
 };
